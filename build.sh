f2cfb8665fe8f88d4315e8e966dc9dbfa5a29820
jpthiery <jpthiery@xebia.fr>
2016-04-29 10:48:18 +0200

    Add build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-DOCKER_BIN_PATH=$(type -a docker | awk '{print $3}')
+DOCKER_BIN_PATH=$(type -a docker | head -n1 | awk '{print $3}')
+
 if [ ! -x "$DOCKER_BIN_PATH" ]; then
   echo "Unable to find a docker executable, please install Docker"
   exit 1
 fi
 
-
 docker run -it --rm  -v /tmp/kodokojo/.m2:/root/.m2 -v "$PWD":/usr/src/mymaven -w /usr/src/mymaven maven:3-jdk-8 mvn install
+rc=$?
+if [[ $rc != 0 ]]; then
+  exit $rc
+fi
+
+
